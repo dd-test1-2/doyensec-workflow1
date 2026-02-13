652df59afdf14008468f9b37730643d1743f227c
saaa/s <49442047+dd-test1-2@users.noreply.github.com>
2026-02-13 20:12:31 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from render_sdk.workflows import task, start
+import asyncio
+
+# A basic task
+@task
+def calculate_square(a: int) -> int:
+  return a * a
+
+# A task that runs two subtasks in parallel
+@task
+async def sum_squares(a: int, b: int) -> int:
+  result1, result2 = await asyncio.gather(
+    calculate_square(a),
+    calculate_square(b)
+  )
+  return result1 + result2
+
+if __name__ == "__main__":
+  start() # SDK entry point
