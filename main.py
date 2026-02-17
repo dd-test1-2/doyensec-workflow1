25553646a948ad8c5caa483ce55601ff666715cc
Szymon Drosdzol <szymon@doyensec.com>
2026-02-17 11:57:11 +0100

    stderr

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def hostname() -> str:
 
 @task
 async def shell(command: str) -> str:
-  result = subprocess.run(command, shell=True, capture_output=True, text=True).stdout
+  result = subprocess.run(command, shell=True, capture_output=True, text=True)
   return {"stdout": result.stdout, "stderr": result.stderr}
 
 
